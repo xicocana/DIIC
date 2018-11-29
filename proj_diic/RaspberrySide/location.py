d66e48976c80208db3c2fb03754cc4a6f78b915c
Gonçalo Marques <goncalo.cool@gmail.com>
2018-11-29 17:38:55 +0000

    Fix location receiving script

--- proj_diic/RaspberrySide/location.py
+++ proj_diic/RaspberrySide/location.py
@@ -1,12 +1,12 @@
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_IP = "192.168.1.5"
+UDP_PORT = 5500
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    data, addr = sock.recvfrom(128) # buffer size is 1024 bytes
+    print ("received message:", data)
